221c5a2a2727f8abe14d141130fa8854e3cf700f
Osaretin Festus Agbonsalo <osaretinfestus8@gmail.com>
2025-10-17 22:23:34 +0100

    Ensure logs dir exists on startup, update .gitignore

--- catapi/wsgi.py
+++ catapi/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys 
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'catapi.settings')
+
+application = get_wsgi_application()
+
+project_root = os.path.dirname(os.path.dirname(__file__))
+logs_dir = os.path.join(project_root, 'logs')
+os.makedirs(logs_dir, exist_ok=True)
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profile_api_project.settings')
 
 application = get_wsgi_application()
